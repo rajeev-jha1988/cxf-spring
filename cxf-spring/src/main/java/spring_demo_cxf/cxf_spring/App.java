db712d9cb21382ae57f29b72c7120ce010098522
rjha <rajeev.jha@tavant.com>
2018-01-01 14:56:45 +0530

    added beans and client
    Change-Id: Ib771b047ff9a266b2a94e165ee1e7a48c53a1529
    Signed-off-by: rjha <rajeev.jha@tavant.com>

--- cxf-spring/src/main/java/spring_demo_cxf/cxf_spring/App.java
+++ cxf-spring/src/main/java/spring_demo_cxf/cxf_spring/App.java
@@ -1,5 +1,9 @@
 package spring_demo_cxf.cxf_spring;
 
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+import com.raj.cxf.service.HelloService;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+    	factory.setServiceClass(HelloService.class);
+    	factory.setAddress("http://localhost:8080/spring-cxf/services/HelloService");
+    	
+    	
+    //	factory.setUsername("restuser");
+    //	factory.setPassword("restuser");
+    	HelloService helloService = (HelloService) factory.create();
+    	 String str=helloService.sayHello("rajeev");
+    	 System.out.println(str);
     }
 }
